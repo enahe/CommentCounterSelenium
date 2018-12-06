19dba7d73d4f4b1fe35a5a00224ece55754cf38e
Eric Nahe <enahe@udel.edu>
2018-12-05 20:04:13 -0500

    a good start

---
+++ seleniumtest.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+option = webdriver.ChromeOptions()
+browser = webdriver.Chrome(executable_path=r"C:/Users/Tommy/chromedriver_win32/chromedriver.exe", chrome_options=option)
+browser.get("http://www.facebook.com")
+
+browser.find_element_by_id("email").clear()
+browser.find_element_by_id("email").send_keys("bbjoe345@gmail.com")
+browser.find_element_by_id("pass").clear()
+browser.find_element_by_id("pass").send_keys("Sandvich12")
+browser.find_element_by_id("loginbutton").click();
+time.sleep(10)
+browser.get("https://www.facebook.com/groups/479270829257357/")
+
